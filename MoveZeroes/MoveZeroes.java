1d431539d7822dae9833a60d31d09cc05e567141
arkhypkryvdiuk <kruvdyk1503@gmail.com>
2025-10-30 22:58:00 +0200

    add new task

---
+++ MoveZeroes/MoveZeroes.java
@@ -0,0 +1,41 @@
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        int pos = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                nums[pos++] = num;
+            }
+        }
+        while (pos < nums.length) {
+            nums[pos++] = 0 ;
+        }
+    }
+    public static void main(String[] args) {
+        MoveZeroes solution = new MoveZeroes();
+        int[] nums = {0, 1, 0, 3, 12};
+        solution.moveZeroes(nums);
+        System.out.println(java.util.Arrays.toString(nums)); // [1, 3, 12, 0, 0]
+    }
+}
+
+
+//Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+//
+//Note that you must do this in-place without making a copy of the array.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [0,1,0,3,12]
+//Output: [1,3,12,0,0]
+//Example 2:
+//
+//Input: nums = [0]
+//Output: [0]
+//
+//
+//Constraints:
+//
+//        1 <= nums.length <= 104
+//        -231 <= nums[i] <= 231 - 1
