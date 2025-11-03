90413ee2a367bb4df6f45b0e88326350b067c5e7
arkhypkryvdiuk <kruvdyk1503@gmail.com>
2025-11-03 17:07:33 +0200

    add new task

---
+++ DecodeString/DecodeString.java
@@ -0,0 +1,68 @@
+import java.util.Stack;
+
+public class DecodeString {
+    public static String decodeString(String s) {
+        Stack<Integer> countStack = new Stack<>();
+        Stack<StringBuilder> stringStack = new Stack<>();
+        StringBuilder current = new StringBuilder();
+        int k = 0;
+
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                k = k * 10 + (c - '0');
+            } else if (c == '[') {
+                countStack.push(k);
+                stringStack.push(current);
+                current = new StringBuilder();
+                k = 0;
+            } else if (c == ']') {
+                StringBuilder decoded = stringStack.pop();
+                int repeatCount = countStack.pop();
+                decoded.append(current.toString().repeat(repeatCount));
+                current = decoded;
+            } else {
+                // додаємо звичайні літери
+                current.append(c);
+            }
+        }
+
+        return current.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(decodeString("3[a]2[bc]"));     // aaabcbc
+        System.out.println(decodeString("3[a2[c]]"));      // accaccacc
+        System.out.println(decodeString("2[abc]3[cd]ef")); // abcabccdcdcdef
+    }
+}
+
+//Given an encoded string, return its decoded string.
+//
+//The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+//
+//You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc. Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there will not be input like 3a or 2[4].
+//
+//The test cases are generated so that the length of the output will never exceed 105.
+//
+//
+//
+//Example 1:
+//
+//Input: s = "3[a]2[bc]"
+//Output: "aaabcbc"
+//Example 2:
+//
+//Input: s = "3[a2[c]]"
+//Output: "accaccacc"
+//Example 3:
+//
+//Input: s = "2[abc]3[cd]ef"
+//Output: "abcabccdcdcdef"
+//
+//
+//Constraints:
+//
+//        1 <= s.length <= 30
+//s consists of lowercase English letters, digits, and square brackets '[]'.
+//s is guaranteed to be a valid input.
+//All the integers in s are in the range [1, 300].
