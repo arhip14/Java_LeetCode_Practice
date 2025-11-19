1ece6626a4c18ed42679ba3887fff97e1c4629f4
arkhypkryvdiuk <kruvdyk1503@gmail.com>
2025-11-19 19:56:41 +0200

    add new task

---
+++ N_thTribonacciNumber/N_thTribonacciNumber.java
@@ -0,0 +1,57 @@
+package N_thTribonacciNumber;
+
+public class N_thTribonacciNumber {
+    public  int n_thTribonacciNumber(int n) {
+        if (n == 0) return 0;
+        if (n == 1 || n ==2) return  1;
+
+        int   t0 = 0;
+        int  t1 =1;
+        int  t2 = 1;
+        int tn = 0;
+
+        for (int i = 0 ; i <= n; i++) {
+            tn = t0 + t1 +t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = tn;
+        }
+        return  tn;
+    }
+    public static void main(String[] args) {
+        N_thTribonacciNumber sol = new N_thTribonacciNumber();
+
+        int n1 = 4;
+        System.out.println("T(" + n1 + ") = " + sol.n_thTribonacciNumber(n1)); // очікується 4
+
+        int n2 = 25;
+        System.out.println("T(" + n2 + ") = " + sol.n_thTribonacciNumber(n2)); // очікується 1389537
+    }
+}
+
+
+//The Tribonacci sequence Tn is defined as follows:
+//
+//T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+//
+//Given n, return the value of Tn.
+//
+//
+//
+//Example 1:
+//
+//Input: n = 4
+//Output: 4
+//Explanation:
+//T_3 = 0 + 1 + 1 = 2
+//T_4 = 1 + 1 + 2 = 4
+//Example 2:
+//
+//Input: n = 25
+//Output: 1389537
+//
+//
+//Constraints:
+//
+//        0 <= n <= 37
+//The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
