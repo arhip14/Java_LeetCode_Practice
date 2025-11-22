1ca755bb2e12d1f0874b527c59451e93891d4e31
arkhypkryvdiuk <kruvdyk1503@gmail.com>
2025-11-22 09:42:38 +0200

    add new task

---
+++ SingleNumber/SingleNumber.java
@@ -0,0 +1,54 @@
+package SingleNumber;
+
+public class SingleNumber {
+    public int  singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SingleNumber sn = new SingleNumber();
+        int[] nums1 = {4, 1, 2, 1, 2};
+        int result1 = sn.singleNumber(nums1);
+        System.out.println(result1);
+
+        int[] nums2 = {4,2,3,4,2,3,1};
+        int result2 = sn.singleNumber(nums2);
+        System.out.println(result2);
+    }
+}
+
+//Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+//
+//You must implement a solution with a linear runtime complexity and use only constant extra space.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [2,2,1]
+//
+//Output: 1
+//
+//Example 2:
+//
+//Input: nums = [4,1,2,1,2]
+//
+//Output: 4
+//
+//Example 3:
+//
+//Input: nums = [1]
+//
+//Output: 1
+//
+//
+//
+//Constraints:
+//
+//        1 <= nums.length <= 3 * 104
+//        -3 * 104 <= nums[i] <= 3 * 104
+//Each element in the array appears twice except for one element which appears only once.
